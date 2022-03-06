9029bfdbc1e4b518e27ae5ac343531faa017df1c
Connor Hollis <connorhollis3d@gmail.com>
2022-03-06 12:54:17 -0700

    deleted unused files and imported css files/ now links highlight/darken when clicked

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function About() {
+function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Me Page</h1>
       <p>
         Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
         Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
@@ -15,3 +15,5 @@ export default function About() {
     </div>
   );
 }
+
+export default About;
